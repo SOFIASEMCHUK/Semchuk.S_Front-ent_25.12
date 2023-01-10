0422610e79f3b1f4cb36c76ac61684b809a876d3
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-01-10 14:33:55 +0200

    change js file in  solution branch

--- index.js
+++ index.js
@@ -2,38 +2,45 @@
 const arr = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
 
 function culc(arr) {
-  let num = 0;
-  for (let item of arr) {
-    if (item >= 0 && item !== NaN && typeof item !== 'boolean') {
-      num += Number(item);
+    let num = 0;
+    for (let item of arr) {
+        if (item !== Number.isNaN && typeof item === 'number') {
+            num += item;
+        }
     }
-  }
-  console.log(num);
+    console.log(num);
 }
 
 culc(arr);
+
 //2
 const num1 = +prompt('Enter number 1(task 2)');
 const num2 = +prompt('Enter number 2(task 2)');
-const operator = prompt('Enter operator: + - * / (task 2)');
+const operator = prompt('Enter operator: + - * / % ^(task 2)');
 
 function doMath(num1, num2, operator) {
-  switch (operator) {
-    case '+':
-      console.log(`${num1} ${operator} ${num2} = ${num1 + num2}`);
-      break;
-    case '-':
-      console.log(`${num1} ${operator} ${num2} = ${num1 - num2}`);
-      break;
-    case '*':
-      console.log(`${num1} ${operator} ${num2} = ${num1 * num2}`);
-      break;
-    case '/':
-      console.log(`${num1} ${operator} ${num2} = ${num1 / num2}`);
-      break;
-    default:
-      break;
-  }
+    switch (operator) {
+        case '+':
+            console.log(`${num1} ${operator} ${num2} = ${num1 + num2}`);
+            break;
+        case '-':
+            console.log(`${num1} ${operator} ${num2} = ${num1 - num2}`);
+            break;
+        case '*':
+            console.log(`${num1} ${operator} ${num2} = ${num1 * num2}`);
+            break;
+        case '/':
+            console.log(`${num1} ${operator} ${num2} = ${num1 / num2}`);
+            break;
+        case '%':
+            console.log(`${num1} ${operator} ${num2} = ${num1 % num2}`);
+            break;
+        case '^':
+            console.log(`${num1} ${operator} ${num2} = ${num1 ** num2}`);
+            break;
+        default:
+            break;
+    }
 }
 doMath(num1, num2, operator);
 
@@ -43,29 +50,32 @@ const vertical = +prompt('Введіть довжину внутрішнього
 const arr2 = [];
 
 function arrDouble(horizontal, vertical, arr2) {
-  for (let m = 0; m < vertical; m++) {
-    arr2[m] = [];
-    for (let n = 0; n < horizontal; n++) {
-      arr2[m][n] = prompt(`Enter ${n} element ${m} line`);
+    for (let m = 0; m < vertical; m++) {
+        arr2[m] = [];
+        for (let n = 0; n < horizontal; n++) {
+            arr2[m][n] = prompt(`Enter ${n} element ${m} line`);
+        }
     }
-  }
 }
+
 arrDouble(horizontal, vertical, arr2);
 console.log(arr2);
 
 //4
 const line = prompt('Enter string (task 4)');
 const remove = prompt('Enter the simvole(-s) you want to delete (task 4)');
+let strocka = '';
 
 function deleteSimvole(line, remove) {
-  line = line.split('');
-  for (let i = 0; i < line.length; i++) {
-    for (let j = 0; j < line.length; j++) {
-      if (line[i] === remove[j] && line[i] !== ' ') {
-        line[i] = '';
-      }
+    strocka = line;
+    strocka = strocka.split('');
+    for (let i = 0; i < strocka.length; i++) {
+        for (let j = 0; j < remove.length; j++) {
+            if (strocka[i] === remove[j]) {
+                strocka[i] = '';
+            }
+        }
     }
-  }
-  console.log(line.join(''));
+    console.log(strocka.join(''));
 }
 deleteSimvole(line, remove);
