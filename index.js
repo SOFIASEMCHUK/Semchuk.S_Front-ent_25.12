14c981dd592199fb37d5a0cfe89ef94b00d89c89
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-01-12 22:21:45 +0200

    change my fault

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const arr = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
 
 function culc(arr) {
     let num = 0;
+    let count = 0;
     for (let item of arr) {
-        if (item !== Number.isNaN && typeof item === 'number') {
+        if (item !== Number.isNaN(item) && typeof item === 'number') {
             num += item;
+            count++;
         }
     }
-    console.log(num);
+    console.log(`${num} / ${count} = ${num / count}`);
 }
 
 culc(arr);
@@ -47,35 +49,33 @@ doMath(num1, num2, operator);
 //3
 const horizontal = +prompt('Введіть довжину зовнішнього масиву (task 3)');
 const vertical = +prompt('Введіть довжину внутрішнього масиву (task 3)');
-const arr2 = [];
 
-function arrDouble(horizontal, vertical, arr2) {
+function arrDouble(horizontal, vertical) {
+    const arr2 = [];
     for (let m = 0; m < vertical; m++) {
         arr2[m] = [];
         for (let n = 0; n < horizontal; n++) {
             arr2[m][n] = prompt(`Enter ${n} element ${m} line`);
         }
     }
+    console.log(arr2);
 }
 
-arrDouble(horizontal, vertical, arr2);
-console.log(arr2);
+arrDouble(horizontal, vertical);
 
 //4
 const line = prompt('Enter string (task 4)');
 const remove = prompt('Enter the simvole(-s) you want to delete (task 4)');
-let strocka = '';
 
 function deleteSimvole(line, remove) {
-    strocka = line;
-    strocka = strocka.split('');
-    for (let i = 0; i < strocka.length; i++) {
+    const deleteWord = line.split('');
+    for (let i = 0; i < deleteWord.length; i++) {
         for (let j = 0; j < remove.length; j++) {
-            if (strocka[i] === remove[j]) {
-                strocka[i] = '';
+            if (deleteWord[i] === remove[j]) {
+                deleteWord[i] = '';
             }
         }
     }
-    console.log(strocka.join(''));
+    console.log(deleteWord.join(''));
 }
 deleteSimvole(line, remove);
