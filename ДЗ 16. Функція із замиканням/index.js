60d01762c6880ba4f5ee84c4efdfbc8a6567b1fa
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-01-13 15:52:59 +0200

    delete prompt, add 'result'

--- ДЗ 16. Функція із замиканням/index.js
+++ ДЗ 16. Функція із замиканням/index.js
@@ -1,17 +1,34 @@
 function getParametr() {
-    const count = +prompt('Enter count parametr');
+    // const count = +prompt('Enter count parametr');
     let sum = 0;
-    let num = null;
-    return function () {
-        if (count !== Number.isNaN) {
-            for (let i = 0; i < count; i++) {
-                num = +prompt('Enter parametr');
-                sum += num;
-                console.log(`sum(${num}) = ${sum}`);
-            }
-        }
-    }
+    const num = 0;
+    return function (num) {
+        sum += num;
+        console.log(`sum(${num}) = ${sum}`);
+    };
 }
 
 const result = getParametr();
-result();
+result(1);
+result(3);
+result(6);
+result(9);
+result(1);
+
+//  function getParametr() {
+//     const count = +prompt('Enter count parametr');
+//     let sum = 0;
+//     let num = null;
+//     return function () {
+//         if (count !== Number.isNaN) {
+//             for (let i = 0; i < count; i++) {
+//                 num = +prompt('Enter parametr');
+//                 sum += num;
+//                 console.log(`sum(${num}) = ${sum}`);
+//             }
+//         }
+//     }
+// }
+
+// const result = getParametr();
+// result();
